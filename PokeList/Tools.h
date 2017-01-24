228a827f4f68f8ba8d997df5a0759c265b7cd674
Jeyaksan RAJARATNAM <r.jeyaksan@outlook.com>
2017-01-24 20:29:58 +0100

    Added icons for Launchscreen.storyboard and added basic static methods in static class Tools

---
+++ PokeList/Tools.h
@@ -0,0 +1,16 @@
+//
+//  Tools.h
+//  PokeList
+//
+//  Created by Jeyaksan RAJARATNAM on 23/01/2017.
+//  Copyright © 2017 grp1.moc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Tools : NSObject
+
+/// Convertis le format HEX (0xFFFFFF) en UIColor
++ (UIColor*) UIColorFromRGB:(unsigned) hexValue;
+
+@end
---
+++ PokeList/Tools.m
@@ -0,0 +1,20 @@
+//
+//  Tools.m
+//  PokeList
+//
+//  Created by Jeyaksan RAJARATNAM on 23/01/2017.
+//  Copyright © 2017 grp1.moc. All rights reserved.
+//
+
+#import "Tools.h"
+
+@implementation Tools
+
++ (UIColor*) UIColorFromRGB:(unsigned) hexValue{
+    return [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 \
+                           green:((float)((hexValue & 0x00FF00) >>  8))/255.0 \
+                            blue:((float)((hexValue & 0x0000FF) >>  0))/255.0 \
+                           alpha:1.0];
+}
+
+@end
