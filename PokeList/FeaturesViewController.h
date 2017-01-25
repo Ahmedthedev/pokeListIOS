b7c4e3be50b67f0d750d42bba09661c4a773a45b
Avatar <ahmedhachemi1994@gmail.com>
2017-01-25 22:35:45 +0100

    link class pokemon with featureviewController

--- PokeList/FeaturesViewController.h
+++ PokeList/FeaturesViewController.h
@@ -7,12 +7,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Pokemon.h"
+#import "PokeDataLayer.h"
 
 @interface FeaturesViewController : UIViewController
 
 @property (retain, nonatomic) IBOutlet UIScrollView *featureScrollView;
 @property (retain, nonatomic) IBOutlet UIView *featureView;
+@property (retain, nonatomic) Pokemon* pokemon;
+
+@property (weak, nonatomic) IBOutlet UILabel *firstType;
+@property (weak, nonatomic) IBOutlet UILabel *secondType;
+@property (weak, nonatomic) IBOutlet UILabel *typeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weight;
+
+@property (weak, nonatomic) IBOutlet UILabel *heightLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *height;
+@property (weak, nonatomic) IBOutlet UILabel *description;
+
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *name;
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil pokemonId:(unsigned short)pokeId;
+
+
 
 @end
