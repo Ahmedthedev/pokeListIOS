3bed1871bba1e0557135ed90e03de07b3c5522cc
Avatar <ahmedhachemi1994@gmail.com>
2017-01-28 12:21:54 +0100

    featureView without txt

---
+++ PokeList/PageControl.h
@@ -0,0 +1,13 @@
+//
+//  PageControl.h
+//  PokeList
+//
+//  Created by apple on 28/01/2017.
+//  Copyright © 2017 grp1.moc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageControl : UIViewController
+
+@end
---
+++ PokeList/PageControl.m
@@ -0,0 +1,37 @@
+//
+//  PageControl.m
+//  PokeList
+//
+//  Created by apple on 28/01/2017.
+//  Copyright © 2017 grp1.moc. All rights reserved.
+//
+
+#import "PageControl.h"
+
+@interface PageControl ()
+
+@end
+
+@implementation PageControl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
